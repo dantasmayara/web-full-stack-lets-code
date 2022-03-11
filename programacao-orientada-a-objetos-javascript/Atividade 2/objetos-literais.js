37160f049126d0dab74504989c298f2cb29a064c
Mayara Dantas <98061188+dantasmayara@users.noreply.github.com>
2022-03-11 02:17:28 -0300

    atual

--- programacao-orientada-a-objetos-javascript/Atividade 2/objetos-literais.js
+++ programacao-orientada-a-objetos-javascript/Atividade 2/objetos-literais.js
@@ -1,20 +1,26 @@
-const pessoa = {};
-pessoa.profissao = "dev";
-pessoa.hobby = "desenhar";
-pessoa.sexo = "feminino";
-
-pessoa.trabalhar = function () {
-    return "projetos";
+const pessoa = {
+    profissao: "dev",
+    hobby: "desenhar",
+    sexo: "feminino",
+    kcal: {
+        min: 0,
+        max: 7000,
+        atual: 0
+    },
+    comer: function() {
+        if (this.kcal.atual == this.kcal.max) {
+            return "Você ingeriu 7.000 kcal e ganhou 1kg! Vá treinar!";
+        } else {
+            return ++this.kcal.atual;
+        }
+    },
+    treinar: function() {
+        if (this.kcal.atual == this.kcal.min) {
+            return "Você gastou 7.000 kcal e eliminou 1kg! Parabéns!";
+        } else {
+            return --this.kcal.atual;
+        }
+    }
 };
 
-pessoa.pintar = function () {
-    return "quadros";
-};
-
-pessoa.treinar = function () {
-    return "volei";
-};
-
-console.log(pessoa);
-console.log(pessoa.sexo, pessoa["hobby"]);
-console.log(pessoa.treinar(), pessoa["trabalhar"]());
+console.log (pessoa);
