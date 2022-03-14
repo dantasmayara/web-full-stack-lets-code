4d52ce16e92476af17e111b462eaa66465cc2f66
Mayara Dantas <98061188+dantasmayara@users.noreply.github.com>
2022-03-14 16:31:21 -0300

    atual

--- programacao-orientada-a-objetos-javascript/Atividade 3/script.js
+++ programacao-orientada-a-objetos-javascript/Atividade 3/script.js
@@ -3,7 +3,7 @@
     2 - implementar um método, onde todos os lançamentos sejam exibidos (extrato).
 */
 
-function Conta(agencia=0, numero=0, digito=0, saldo=0, titular=null, tipo="CC", lancamentos=[]) {
+function Conta(agencia=0, numero=0, digito=0, saldo=0, titular=null, tipo="CC") {
   this.agencia = agencia;
   this.numero = numero;
   this.digito = digito;
@@ -17,8 +17,8 @@ Conta.prototype.banco = "C6 Bank";
 
 Conta.prototype.depositar = function(valor) {
   this.saldo += valor;
-  this.lancamentos.push(new this.Lancar(valor, "Depósito", this.saldo, this.conta));
-  return [this.saldo];
+  this.lancamentos.push(new this.Lancar(valor, "Depósito", this.saldo, this.conta, this.contaOrigem));
+  return this.saldo;
 }
 
 Conta.prototype.sacar = function(valor) {
@@ -52,8 +52,8 @@ Conta.prototype.Lancar = function(valor, lancamentos, saldo, contaDestino= null,
   this.saldoFinal = saldo;
 }
 
-Conta.prototype.extrato = function(lancamentos) {
-  return this.lancamentos
+Conta.prototype.extrato = function() {
+  console.log (this.lancamentos);
 }
 
 const contaJoao = new Conta (336, 1234, 0, 1500, "Joao");
@@ -67,7 +67,6 @@ console.log(contaJoao.sacar(1900));
 console.log(contaJoao.sacar(2));
 console.log(contaJoao.transferir(98, contaMaria));
 
-// Conta da Maria
 console.log(contaMaria);
 console.log(contaMaria.extrato());
 console.log(contaMaria.banco);
@@ -75,4 +74,4 @@ console.log(contaMaria.depositar(225));
 console.log(contaMaria.sacar(3200));
 console.log(contaMaria.sacar(25));
 console.log(contaMaria.transferir(50, contaJoao));
-console.log(contaMaria.extrato(lancamentos));
+console.log(contaMaria.extrato());
